56b6384a801770bb9aa334ff08cf7687b47f9184
Rajeshwari Argulwar <98045986+Rajeshwari-Argulwar@users.noreply.github.com>
2024-02-25 11:22:58 +0530

    Create p39.java

---
+++ p39.java
@@ -0,0 +1,24 @@
+/*
+Author :- Rajeshwari
+Write a program to print matrics Left to right 
+purpose :- 2D Array 
+Date:- 23/2/24
+*/
+public class p39
+{
+    public static void main(String[] args) 
+    {   
+        int arr[][]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        for(int i=0;i<=3;i++)
+        {
+            for(int j=0;j<=3;j++)
+            {
+                if(i>=1){
+                    System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    
+    }
+}
+    
